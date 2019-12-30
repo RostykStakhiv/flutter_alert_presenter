51c29c10912f1cdae929dd94088bfdef3f7a8dd2
Rostyslav Stakhiv <rost.stakhiv@gmail.com>
2019-12-30 23:12:58 +0200

    - Created alert presenter;

--- flutter_alert_presenter/lib/flutter_alert_presenter.dart
+++ flutter_alert_presenter/lib/flutter_alert_presenter.dart
@@ -1,7 +1,3 @@
 library flutter_alert_presenter;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:flutter_alert_presenter/alert_presenter.dart';
